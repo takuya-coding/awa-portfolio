bda793b0357a618b6a629eca88a7836d0f0f0a9d
takuya <awa08082023@gmail.com>
2023-12-30 05:29:16 +0900

    footer.php作成

---
+++ WordPressTheme/assets/footer.php
@@ -0,0 +1,93 @@
+<footer class="footer layout-footer">
+    <div class="footer__inner inner">
+        <div class="footer__head">
+            <div class="footer__logo">
+                <a href="#"><img src="./assets/images/common/logo.svg" alt="CodeUps"></a>
+            </div>
+            <div class="footer__icon">
+                <a href="#" class="footer__icon-facebook"><img src="./assets/images/common/facebook.svg"
+                        alt="facebookアイコン"></a>
+                <a href="#" class="footer__icon-instagram"><img src="./assets/images/common/instagram.svg"
+                        alt="instagramアイコン"></a>
+            </div>
+        </div>
+        <div class="footer__nav common-nav">
+            <div class="common-nav__inner">
+                <ul class="common-nav__items">
+                    <li class="common-nav__item-wrap">
+                        <div class="common-nav__item-wrap-for-pc">
+                            <div class="common-nav__item">
+                                <a href="archive-campaign.html">キャンペーン</a>
+                                <ul class="common-nav__lv2-items">
+                                    <li class="common-nav__lv2-item"><a href="archive-campaign.html">ライセンス講習</a></li>
+                                    <li class="common-nav__lv2-item"><a href="archive-campaign.html">ファンダイビング</a></li>
+                                    <li class="common-nav__lv2-item"><a href="archive-campaign.html">体験ダイビング</a></li>
+                                </ul>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="page-about.html">私たちについて</a>
+                            </div>
+                        </div>
+                        <div class="common-nav__item-wrap-for-pc">
+                            <div class="common-nav__item">
+                                <a href="page-diving-info.html">ダイビング情報</a>
+                                <ul class="common-nav__lv2-items">
+                                    <li class="common-nav__lv2-item"><a
+                                            href="page-diving-info.html?active-tab=tab-1">ライセンス講習</a></li>
+                                    <li class="common-nav__lv2-item"><a
+                                            href="page-diving-info.html?active-tab=tab-2">ファンダイビング</a></li>
+                                    <li class="common-nav__lv2-item"><a
+                                            href="page-diving-info.html?active-tab=tab-3">体験ダイビング</a></li>
+                                </ul>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="home.html">ブログ</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="common-nav__item-wrap">
+                        <div class="common-nav__item-wrap-for-pc">
+                            <div class="common-nav__item">
+                                <a href="archive-voices.html">お客様の声</a>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="page-price.html">料金一覧</a>
+                                <ul class="common-nav__lv2-items">
+                                    <li class="common-nav__lv2-item"><a href="page-price.html">ライセンス講習</a></li>
+                                    <li class="common-nav__lv2-item"><a href="page-price.html">体験ダイビング</a></li>
+                                    <li class="common-nav__lv2-item"><a href="page-price.html">ファンダイビング</a></li>
+                                    <li class="common-nav__lv2-item"><a href="page-price.html">スペシャルダイビング</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="common-nav__item-wrap-for-pc">
+                            <div class="common-nav__item">
+                                <a href="page-faq.html">よくある質問</a>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="page-sitemap.html">サイトマップ</a>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="page-privacy-policy.html">プライバシー<br>ポリシー</a>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="page-terms.html">利用規約</a>
+                            </div>
+                            <div class="common-nav__item">
+                                <a href="page-contact.html">お問い合わせ</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <small
+            class="footer__copyright">copyright&nbsp;&copy;&nbsp;2021&nbsp;&#045;&nbsp;2023&nbsp;codeups&nbsp;LLC&#046;&nbsp;all&nbsp;rights&nbsp;reserved&#046;</small>
+    </div>
+</footer>
+
+<button class="to-top js-to-top"></button>
+<?php wp_footer(); ?>
+</body>
+
+</html>
