97ad2121131f2e105dd9991b27875290ede06341
takuya <awa08082023@gmail.com>
2023-12-31 12:45:03 +0900

    functions.php改めて書き直す

--- WordPressTheme/assets/functions.php
+++ WordPressTheme/assets/functions.php
@@ -19,14 +19,17 @@ function my_script_init()
 { // WordPressに含まれているjquery.jsを読み込まない
     wp_deregister_script('jquery');
     // jQueryの読み込み
-    wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.6.0.js', "", "1.0.1");
-    wp_enqueue_script( 'swiper', '//cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', "", "1.0.1", true );
-    wp_enqueue_script('inview', get_template_directory_uri() . './assets/js/jquery.inview.min.js', array('jquery'), '1.0.1', true);
-    wp_enqueue_style( 'gotu-font', 'https://fonts.googleapis.com/css2?family=Gotu&display=swap', false );
-    wp_enqueue_style( 'lato-font', 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap', false );
-    wp_enqueue_style( 'noto-sans-jp-font', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap', false );
-    wp_enqueue_script('main-js', get_template_directory_uri() . './assets/js/script.js', array('jquery'), '1.0.1', true);
-    wp_enqueue_style('swiper-css', '//cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', array(), '1.0.1');
-    wp_enqueue_style('style-css', get_template_directory_uri() . './assets/css/style.css', array(), '1.0.1');
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.0.js', array(), '3.6.0');
+    wp_enqueue_script( 'swiper', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', array(), '8.0.0', true );
+    wp_enqueue_script('inview', get_template_directory_uri() . '/assets/js/jquery.inview.min.js', array('jquery'), '1.0.1', true);
+    // Gotuフォントの追加
+    wp_enqueue_style( 'Gotu', '//fonts.googleapis.com/css2?family=Gotu&display=swap' );
+    // Latoフォントの追加（400と700の太さ）
+    wp_enqueue_style( 'Lato', '//fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap' );
+    // Noto Sans JPフォントの追加（400, 500, 700の太さ）
+    wp_enqueue_style( 'NotoSansJP', '//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap' );
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0.1', true);
+    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', array(), '8.0.0');
+    wp_enqueue_style('style-css', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.1');
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
