87a9366ce97aa969e1605f05a53af08b9caba1f6
takuya <awa08082023@gmail.com>
2023-12-31 16:40:26 +0900

    page-faq.php記述

---
+++ WordPressTheme/assets/page-faq.php
@@ -0,0 +1,135 @@
+<?php get_header(); ?>
+<main>
+    <div class="main-view">
+        <div class="main-view__inner">
+            <h1 class="main-view__title main-view__title--uppercase">faq</h1>
+            <div class="main-view__img">
+                <picture>
+                    <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/images/common/faq-mv-pc.webp"
+                        media="(min-width:768px)" type="image/webp" />
+                    <img src="<?php echo esc_url(get_theme_file_uri()); ?>/images/common/faq-mv-sp.webp"
+                        alt="透き通るコバルトブルーの海と白い砂浜" width="375" height="460" decoding="async">
+                </picture>
+            </div>
+        </div>
+    </div>
+
+    <!-- breadcrumb -->
+    <div class="breadcrumbs layout-breadcrumbs">
+        <div class="breadcrumbs__inner inner">
+            <!-- Breadcrumb NavXT -->
+            <span>
+                <a href="index.html" class="home">
+                    <span>top</span>
+                </a>
+            </span>
+            <span>&gt;</span>
+            <span>
+                <span class="post post-page current-item">よくある質問</span>
+            </span>
+        </div>
+    </div>
+
+    <section class="faq-accordion js-faq-accordion layout-faq-accordion">
+        <div class="faq-accordion__inner inner">
+            <div class="faq-accordion__container">
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+                <div class="faq-accordion__item js-faq-accordion-item">
+                    <h2 class="faq-accordion__title js-faq-accordion-title">ここに質問が入ります。</h2>
+                    <div class="faq-accordion__content js-faq-accordion-content">
+                        <p class="faq-accordion__text">
+                            ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="common-contact layout-common-contact layout-common-contact--margin" id="contact">
+        <div class="common-contact__inner inner">
+            <div class="common-contact__wrapper">
+                <div class="common-contact__shop-info">
+                    <div class="common-contact__logo">
+                        <img src="<?php echo esc_url(get_theme_file_uri()); ?>/images/common/logo.svg" alt="CodeUps">
+                    </div>
+                    <div class="common-contact__shop-info-wrapper">
+                        <div class="common-contact__shop-info-text">
+                            <p class="common-contact__address">沖縄県那覇市1&#045;1</p>
+                            <a href="tel:+81-120-000-0000"
+                                class="common-contact__tel">tel&#058;0120&#045;000&#045;0000</a>
+                            <p class="common-contact__business-hours">営業時間&#058;8&#058;30&#045;19&#058;00</p>
+                            <p class="common-contact__regular-holiday">定休日&#058;毎週火曜日</p>
+                        </div>
+                        <div class="common-contact__shop-info-map">
+                            <div class="common-contact__map">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.1927241401127!2d127.69486729999998!3d26.22292675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e5696278d6553b%3A0xcb44277c653ed00b!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yR!5e0!3m2!1sja!2sjp!4v1692299234684!5m2!1sja!2sjp"
+                                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="common-contact__more">
+                    <div class="common-contact__title section-title">
+                        <p class="section-title__en section-title__en--size">contact</p>
+                        <h2 class="section-title__ja section-title__ja--position">お問い合わせ</h2>
+                    </div>
+                    <p class="common-contact__text">ご予約・お問い合わせはコチラ</p>
+                    <div class="common-contact__button">
+                        <a href="#" class="button">contact&nbsp;us<span class="button__arrow"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
