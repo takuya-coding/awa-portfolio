8658361c38b2290ebcb80a5b3e4d6f6ea9740cd9
takuya <awa08082023@gmail.com>
2024-01-09 07:40:19 +0900

    SCFに関する記述削除（CFS使用のため）

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -75,7 +75,7 @@ function getPostViews($postID) {
 
 
   
-// campaingページに表示する最大投稿数を変更するための記述
+// campaignページに表示する最大投稿数を変更するための記述
   function custom_posts_per_page($query)
   {
       if (!is_admin() && $query->is_main_query()) {
@@ -87,17 +87,3 @@ function getPostViews($postID) {
       }
   }
   add_action('pre_get_posts', 'custom_posts_per_page');
-
-
-
-
-  // SCFに関する記述
-  /**
- * @param string $page_title ページのtitle属性値
- * @param string $menu_title 管理画面のメニューに表示するタイトル
- * @param string $capability メニューを操作できる権限（manage_options とか）
- * @param string $menu_slug オプションページのスラッグ。ユニークな値にすること。
- * @param string|null $icon_url メニューに表示するアイコンの URL
- * @param int $position メニューの位置
- */
-SCF::add_options_page( 'CodeUps-diving-shop', 'ギャラリー', 'manage_options', 'theme-options' );
