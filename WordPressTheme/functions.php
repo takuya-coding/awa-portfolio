45c347ab58dc774fa38f92e98d0fb4233a6ca1d4
takuya <awa08082023@gmail.com>
2024-01-13 12:47:49 +0900

    Contact Form 7のショートコードコピペ

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -87,3 +87,12 @@ function getPostViews($postID) {
       }
   }
   add_action('pre_get_posts', 'custom_posts_per_page');
+
+
+
+
+// Contact Form 7で自動挿入されるPタグ、brタグを削除
+add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+function wpcf7_autop_return_false() {
+  return false;
+}
