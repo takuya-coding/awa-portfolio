131fc16c39504a606b35ff45911683f6350444bf
takuya <awa08082023@gmail.com>
2024-01-15 05:07:58 +0900

    404.php作成

---
+++ WordPressTheme/404.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+<main>
+    <!-- breadcrumb -->
+    <div class="breadcrumbs layout-breadcrumbs breadcrumbs--color layout-breadcrumbs--padding">
+        <div class="breadcrumbs__inner inner">
+            <!-- Breadcrumb NavXT -->
+            <span>
+                <a href="index.html" class="home">
+                    <span>top</span>
+                </a>
+            </span>
+            <span>&gt;</span>
+            <span>
+                <span class="post post-page current-item">404</span>
+            </span>
+        </div>
+    </div>
+
+    <div class="page-404">
+        <div class="page-404__inner inner">
+            <h1 class="page-404__title">404</h1>
+            <p class="page-404__text">申し訳ありません。<br>お探しのページが見つかりません。</p>
+            <div class="page-404__404-button">
+                <a href="index.html" class="button button--color">page&nbsp;<span>top</span><span
+                        class="button__arrow button__arrow--color"></span></a>
+            </div>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php if (is_404()) : ?>
+    <meta http-equiv="refresh" content=" 5; url=<?php echo esc_url(home_url("")); ?>">
+    <?php endif; ?>
     <?php wp_head(); ?>
 </head>
 
