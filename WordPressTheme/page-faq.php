3ec3b689aca82005ab98ef900d2dc224e07bea7f
takuya <awa08082023@gmail.com>
2024-01-16 12:27:53 +0900

    contactセクションの共通化

--- WordPressTheme/page-faq.php
+++ WordPressTheme/page-faq.php
@@ -27,44 +27,8 @@
         </div>
     </section>
 
-    <section class="common-contact layout-common-contact layout-common-contact--margin" id="contact">
-        <div class="common-contact__inner inner">
-            <div class="common-contact__wrapper">
-                <div class="common-contact__shop-info">
-                    <div class="common-contact__logo">
-                        <img src="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/logo.svg"
-                            alt="CodeUps">
-                    </div>
-                    <div class="common-contact__shop-info-wrapper">
-                        <div class="common-contact__shop-info-text">
-                            <p class="common-contact__address">沖縄県那覇市1&#045;1</p>
-                            <a href="tel:+81-120-000-0000"
-                                class="common-contact__tel">tel&#058;0120&#045;000&#045;0000</a>
-                            <p class="common-contact__business-hours">営業時間&#058;8&#058;30&#045;19&#058;00</p>
-                            <p class="common-contact__regular-holiday">定休日&#058;毎週火曜日</p>
-                        </div>
-                        <div class="common-contact__shop-info-map">
-                            <div class="common-contact__map">
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.1927241401127!2d127.69486729999998!3d26.22292675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e5696278d6553b%3A0xcb44277c653ed00b!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yR!5e0!3m2!1sja!2sjp!4v1692299234684!5m2!1sja!2sjp"
-                                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="common-contact__more">
-                    <div class="common-contact__title section-title">
-                        <p class="section-title__en section-title__en--size">contact</p>
-                        <h2 class="section-title__ja section-title__ja--position">お問い合わせ</h2>
-                    </div>
-                    <p class="common-contact__text">ご予約・お問い合わせはコチラ</p>
-                    <div class="common-contact__button">
-                        <a href="#" class="button">contact&nbsp;us<span class="button__arrow"></span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <!-- contact-section -->
+    <?php get_template_part('template-parts/section-contact'); ?>
+
 </main>
 <?php get_footer(); ?>
