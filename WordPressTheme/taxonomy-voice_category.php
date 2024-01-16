346b34e969c78eac34f2b04c4719d6d6a2486fb1
takuya <awa08082023@gmail.com>
2024-01-17 07:39:03 +0900

    taxonomy-voice_category.php記述途中まで

--- WordPressTheme/taxonomy-voice_category.php
+++ WordPressTheme/taxonomy-voice_category.php
@@ -21,16 +21,66 @@
     <section class="voice layout-voice">
         <div class="voice__inner inner">
             <div class="voice__category category">
-                <a href="" class="category__button is-active">all</a>
+                <!-- <a href="" class="category__button is-active">all</a>
                 <a href="" class="category__button">ライセンス講習</a>
                 <a href="" class="category__button">ファンダイビング</a>
-                <a href="" class="category__button">体験ダイビング</a>
+                <a href="" class="category__button">体験ダイビング</a> -->
+                <?php
+                    $current_term_id = get_queried_object()->term_id;
+                    $terms = get_terms(array(
+                        // 表示するタクソノミースラッグを記述
+                        'taxonomy' => 'voice_category',
+                        'orderby' => 'name',
+                        'order'   => 'ASC',
+                        // 表示するタームの数を指定
+                        'number'  => 5
+                    ));
+                    
+                    // カスタム投稿一覧ページへのURL
+                    $home_class = (is_post_type_archive()) ? 'is-active' : '';
+                    $home_link = sprintf(
+                        //カスタム投稿一覧ページへのaタグに付与するクラスを指定できる
+                        '<a class="category__button %s" href="%s" alt="%s">all</a>',
+                        $home_class,
+                        // カスタム投稿一覧ページのスラッグを指定
+                        esc_url(home_url('/voice')),
+                        esc_attr(__('View all posts', 'textdomain'))
+                    );
+                    echo sprintf(esc_html__('%s', 'textdomain'), $home_link);
+                    
+                    // タームのリンク
+                    if ($terms) {
+                        foreach ($terms as $term) {
+                            // カレントクラスに付与するクラスを指定できる
+                            $term_class = ($current_term_id === $term->term_id) ? 'is-active' : '';
+                            $term_link = sprintf(
+                                // 各タームに付与するクラスを指定できる
+                                '<a class="category__button %s" href="%s" alt="%s">%s</a>',
+                                $term_class,
+                                esc_url(get_term_link($term->term_id)),
+                                esc_attr(sprintf(__('View all posts in %s', 'textdomain'), $term->name)),
+                                esc_html($term->name)
+                            );
+                            echo sprintf(esc_html__('%s', 'textdomain'), $term_link);
+                        }
+                    }
+                    ?>
             </div>
 
             <?php
+            // タクソノミーのスラッグを指定
+            $voice_category_slug = get_query_var('voice_category');
             $args = array(
+                // カスタム投稿のスラッグを指定
                 "post_type" => "voice",
-                "posts_per_page" => 6
+                'tax_query' => array(
+                    array(
+                        // タクソノミーのスラッグを指定
+                        'taxonomy' => 'voice_category',
+                        'field'    => 'slug',
+                        'terms'    => $voice_category_slug,
+                    ),
+                ),
             );
             $the_query = new WP_Query($args);
             ?>
