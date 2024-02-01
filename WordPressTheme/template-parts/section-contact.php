d4fa017d59b22c50445f6ab80fdc6dff0b3881ea
takuya <awa08082023@gmail.com>
2024-02-02 05:33:01 +0900

    section-contact.phpのカスタムフィールドをフロントページ以外でも呼び出す

--- WordPressTheme/template-parts/section-contact.php
+++ WordPressTheme/template-parts/section-contact.php
@@ -9,30 +9,33 @@
                 <div class="common-contact__shop-info-wrapper">
                     <div class="common-contact__shop-info-text">
                         <?php
-                        // 表示するページまたは投稿のIDを取得し、get-field関数でカスタムフィールドの値を取得
-                        global $post;
+                        // get_query_varを使用して渡されたIDを取得（フロントページに設定したカスタムフィールドの情報を受け取る）
+                        $contact_page_id = get_query_var('contact_page_id', false);
+
+                        // カスタムフィールドの値を取得するためのIDを設定（フロントページに設定したカスタムフィールドの値を取得）
+                        $id_to_use = $contact_page_id ? $contact_page_id : $post->ID;
 
                         // 住所の表示
-                        $address = get_field('contact-address', $post->ID);
+                        $address = get_field('contact-address', $id_to_use);
                         if ($address) {
                             // esc_html関数を使用して安全なHTMLとして出力
                             echo '<p class="common-contact__address">' . esc_html($address) . '</p>';
                         }
 
                         // 電話番号の表示
-                        $tel = get_field('contact-tel', $post->ID);
+                        $tel = get_field('contact-tel', $id_to_use);
                         if ($tel) {
                             echo '<a href="tel:' . esc_attr($tel) . '" class="common-contact__tel">tel&#058;' . esc_html($tel) . '</a>';
                         }
 
                         // 営業時間の表示
-                        $business_hours = get_field('contact-business-hours', $post->ID);
+                        $business_hours = get_field('contact-business-hours', $id_to_use);
                         if ($business_hours) {
                             echo '<p class="common-contact__business-hours">営業時間&#058;' . esc_html($business_hours) . '</p>';
                         }
 
                         // 定休日の表示
-                        $regular_holiday = get_field('contact-regular-holiday', $post->ID);
+                        $regular_holiday = get_field('contact-regular-holiday', $id_to_use);
                         if ($regular_holiday) {
                             echo '<p class="common-contact__regular-holiday">定休日&#058;' . esc_html($regular_holiday) . '</p>';
                         }
@@ -47,7 +50,7 @@
                             <?php
                             global $post;
                             // Googleマップのiframeの表示
-                            $map = get_field('contact-map', $post->ID);
+                            $map = get_field('contact-map', $id_to_use);
                             if ($map) {
                                 // wp_kses_post関数は、投稿内容に許可されたHTMLタグのみを含めるようにフィルタリングする。→iframeタグはデフォルトで許可されていないため、カスタムフィルタリングルールを定義する必要がある。
                                 echo wp_kses($map, array(
