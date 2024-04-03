4ad21d80774c41c52fa1a912e2fa84590774f441
takuya <awa08082023@gmail.com>
2024-04-04 07:24:30 +0900

    PF化：voiceページ-mv画像差し替え

--- WordPressTheme/template-parts/main-view.php
+++ WordPressTheme/template-parts/main-view.php
@@ -77,8 +77,8 @@ if (!is_page('404')) {
                     echo '<img src="' . esc_url(get_theme_file_uri()) . '/assets/images/common/PF-mv6-sp.webp" alt="海の中を優雅に泳ぐサメ" width="375" height="460" decoding="async">';
                 // voiceページおよびサブカテゴリ（タクソノミー）のチェック
                 } elseif (is_post_type_archive('voice') || is_tax('voice_category')) {
-                    echo '<source srcset="' . esc_url(get_theme_file_uri()) . '/assets/images/common/PF-mv1-pc.webp" media="(min-width:768px)" type="image/webp" />';
-                    echo '<img src="' . esc_url(get_theme_file_uri()) . '/assets/images/common/PF-mv1-sp.webp" alt="きれいな海面に5人のダイバーが浮いている" width="375" height="460" decoding="async">';
+                    echo '<source srcset="' . esc_url(get_theme_file_uri()) . '/assets/images/common/PF-mv7-pc.webp" media="(min-width:768px)" type="image/webp" />';
+                    echo '<img src="' . esc_url(get_theme_file_uri()) . '/assets/images/common/PF-mv7-sp.webp" alt="きれいな海面に5人のダイバーが浮いている" width="375" height="460" decoding="async">';
                 } elseif (is_page('price')) {
                     echo '<source srcset="' . esc_url(get_theme_file_uri()) . '/assets/images/common/price-mv-pc.webp" media="(min-width:768px)" type="image/webp" />';
                     echo '<img src="' . esc_url(get_theme_file_uri()) . '/assets/images/common/price-mv-sp.webp" alt="水面から頭だけ出した一人のダイバー" width="375" height="460" decoding="async">';
