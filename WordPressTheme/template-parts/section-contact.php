6550f4357bc4aae0e097932535e344689bdafe60
takuya <awa08082023@gmail.com>
2024-04-10 03:57:27 +0900

    contact修正

--- WordPressTheme/template-parts/section-contact.php
+++ WordPressTheme/template-parts/section-contact.php
@@ -3,8 +3,7 @@
         <div class="common-contact__wrapper">
             <div class="common-contact__shop-info">
                 <div class="common-contact__logo">
-                    <img src="<?php echo esc_url(get_theme_file_uri( "/assets/images/common/PF-shop-logo.webp" )); ?>"
-                        alt="blue wave">
+                    <img src="<?php echo esc_url(get_theme_file_uri( "/assets/images/common/PF-shop-logo.webp" )); ?>" alt="blue wave">
                 </div>
                 <div class="common-contact__shop-info-wrapper">
                     <div class="common-contact__shop-info-text">
@@ -40,10 +39,6 @@
                             echo '<p class="common-contact__regular-holiday">定休日&#058;' . esc_html($regular_holiday) . '</p>';
                         }
                         ?>
-                        <!-- <p class="common-contact__address">沖縄県那覇市1&#045;1</p> -->
-                        <!-- <a href="tel:+81-120-000-0000" class="common-contact__tel">tel&#058;0120&#045;000&#045;0000</a> -->
-                        <!-- <p class="common-contact__business-hours">営業時間&#058;8&#058;30&#045;19&#058;00</p> -->
-                        <!-- <p class="common-contact__regular-holiday">定休日&#058;毎週火曜日</p> -->
                     </div>
                     <div class="common-contact__shop-info-map">
                         <div class="common-contact__map">
@@ -65,10 +60,6 @@
                                 ));
                             }
                             ?>
-                            <!-- <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.1927241401127!2d127.69486729999998!3d26.22292675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e5696278d6553b%3A0xcb44277c653ed00b!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yR!5e0!3m2!1sja!2sjp!4v1692299234684!5m2!1sja!2sjp"
-                                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade"></iframe> -->
                         </div>
                     </div>
                 </div>
@@ -80,9 +71,7 @@
                 </div>
                 <p class="common-contact__text">ご予約・お問い合わせはコチラ</p>
                 <div class="common-contact__button">
-                    <a href="<?php echo esc_url(home_url("/contact")) ?>"
-                        class="button button--common-contact">contact&nbsp;us<span
-                            class="button__arrow button__arrow--common-contact"></span></a>
+                    <a href="<?php echo esc_url(home_url("/contact")) ?>" class="button button--common-contact">contact&nbsp;us<span class="button__arrow button__arrow--common-contact"></span></a>
                 </div>
             </div>
         </div>
